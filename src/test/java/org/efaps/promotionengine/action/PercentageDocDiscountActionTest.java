47cecea6a2c8830535acd22c288894c103920424
Jan Moxter <jan@moxter.net>
2023-10-25 01:55:08 -0500

    Adding test

--- src/test/java/org/efaps/promotionengine/action/PercentageDocDiscountActionTest.java
+++ src/test/java/org/efaps/promotionengine/action/PercentageDocDiscountActionTest.java
@@ -5,18 +5,18 @@ import java.math.BigDecimal;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class FixedDocDiscountActionTest
+public class PercentageDocDiscountActionTest
 {
     @Test
     public void equalsTest() {
-        final var obj1 = new PercentageDiscountAction();
+        final var obj1 = new PercentageDocDiscountAction();
         Assert.assertTrue(obj1.equals(obj1));
         Assert.assertFalse(obj1.equals("different"));
 
-        Assert.assertTrue(obj1.equals(new PercentageDiscountAction()));
+        Assert.assertTrue(obj1.equals(new PercentageDocDiscountAction()));
 
         obj1.setPercentage(new BigDecimal(10));
-        final var obj2 = new PercentageDiscountAction().setPercentage(new BigDecimal(10));
+        final var obj2 = new PercentageDocDiscountAction().setPercentage(new BigDecimal(10));
         Assert.assertTrue(obj1.equals(obj2));
     }
 }
