409292e11de5afb55d8cc5e62831297f356bd206
Jan Moxter <jan@moxter.net>
2023-10-25 01:20:21 -0500

    Test the equals

---
+++ src/test/java/org/efaps/promotionengine/action/FixedDocDiscountActionTest.java
@@ -0,0 +1,22 @@
+package org.efaps.promotionengine.action;
+
+import java.math.BigDecimal;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FixedDocDiscountActionTest
+{
+    @Test
+    public void equalsTest() {
+        final var obj1 = new PercentageDiscountAction();
+        Assert.assertTrue(obj1.equals(obj1));
+        Assert.assertFalse(obj1.equals("different"));
+
+        Assert.assertTrue(obj1.equals(new PercentageDiscountAction()));
+
+        obj1.setPercentage(new BigDecimal(10));
+        final var obj2 = new PercentageDiscountAction().setPercentage(new BigDecimal(10));
+        Assert.assertTrue(obj1.equals(obj2));
+    }
+}
